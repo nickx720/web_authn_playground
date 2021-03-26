f6dd63b4acaa201a517e7e4e86f45e2f5f629274
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-03-26 15:25:23 -0400

    Domain mapping

--- test-webauthn/src/main.rs
+++ test-webauthn/src/main.rs
@@ -133,7 +133,7 @@ async fn main() -> std::io::Result<()> {
         String::from("/"),
         String::from("app-73b3686f-584b-49dd-966f-b38af14429d1"),
         String::from("https://app-73b3686f-584b-49dd-966f-b38af14429d1.cleverapps.io"),
-        String::from("app-73b3686f-584b-49dd-966f-b38af14429d1"),
+        String::from("app-73b3686f-584b-49dd-966f-b38af14429d1.cleverapps.io"),
         String::from("app-73b3686f-584b-49dd-966f-b38af14429d1:80"),
     );
     let prefix = cmd_opt.prefix.clone();
