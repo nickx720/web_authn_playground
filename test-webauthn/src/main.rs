fcd3fc8b629bfd1c6a0c9db0bb2adfdcd068d740
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-03-26 11:37:10 -0400

    Docker Container

--- test-webauthn/src/main.rs
+++ test-webauthn/src/main.rs
@@ -170,7 +170,7 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/build", "public/build").show_files_listing())
             .service(Files::new("/", "./public/").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
